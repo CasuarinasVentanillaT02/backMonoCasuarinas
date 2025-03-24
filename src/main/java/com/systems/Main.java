950eb21d87cb0edf038498cb0beb97e7fa62a4f2
CasuarinasVentanillaT02 <casuarinasventanillat02@gmail.com>
2025-03-23 21:48:33 -0500

    FuentesJasper.Add.V006

--- src/main/java/com/systems/Main.java
+++ src/main/java/com/systems/Main.java
@@ -4,17 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Main {
-	static {
-		try {
-			// Forzar la inicialización de la clase para evitar NoClassDefFoundError
-			Class.forName("net.sf.jasperreports.engine.util.JRStyledTextParser");
-		} catch (ClassNotFoundException e) {
-			throw new RuntimeException("Error loading JasperReports class", e);
-		}
-	}
+public class Main {	
 	public static void main(String[] args) {
-		System.setProperty("java.awt.headless", "true");
 		SpringApplication.run(Main.class, args);
 	}
 
